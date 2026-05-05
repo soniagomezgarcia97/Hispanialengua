f29a2e063f36d2ac24622afa0ec7c0df8f17ebb5
Sonia Gomez <soniagomez@artelnics.com>
2026-05-05 09:54:19 +0200

    correccion responsive

--- src/components/Trabalenguas.tsx
+++ src/components/Trabalenguas.tsx
@@ -15,13 +15,13 @@ const TrabalenguasComp = ({ titulo, texto, urlImagen }: PropsType) => {
                 <h2 className="text-white font-bold text-3xl drop-shadow-[0_2px_4px_rgba(0,0,0,0.8)] bg-black/40 px-3 py-1 rounded-md backdrop-blur-sm">{titulo}</h2>
             </button>
             {abrirTrabalenguas && (
-                <div className="fixed z-10 top-1/2 left-1/2 w-[90%] -translate-x-1/2 -translate-y-1/2 max-h-[100vh] overflow-y-auto lg:h-[80vh] flex flex-col justify-start items-center bg-[#1c1a16]/80 rounded-md backdrop-blur-sm p-5">
+                <div className="fixed z-10 top-1/2 left-1/2 w-[90%] -translate-x-1/2 -translate-y-1/2 max-h-[100vh] overflow-y-auto flex flex-col justify-start items-center bg-[#1c1a16]/80 rounded-md backdrop-blur-sm p-5">
                     <button className="text-white bg-yellow-500 drop-shadow-[0_2px_4px_rgba(0,0,0,0.8)] bg-red/60 px-3 py-1 rounded-md backdrop-blur-sm mb-7" onClick={() => { setAbrirTrabalenguas(false) }}>Cerrar</button>
                     {/* Info */}
                     <div className="w-full flex flex-col items-center gap-2.5">
                         <h2 className="text-4xl text-white font-bold mb-5">{titulo}</h2>
                         <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4">
-                            <div className="w-full aspect-square bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${urlImagen})` }}></div>
+                            <div className="w-full xl:w-[60vh] xl:mx-auto aspect-square bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${urlImagen})` }}></div>
                             <p className="text-2xl text-white/80 text-justify w-[90%] self-start mx-auto">{texto}</p>
                         </div>
                     </div>
