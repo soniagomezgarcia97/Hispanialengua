43e1c619c1f9a63185082548350c08788d9d76d4
Sonia Gomez <soniagomez@artelnics.com>
2026-05-15 15:16:25 +0200

    mejora diseño dapsulas

--- src/components/Capsulas.tsx
+++ src/components/Capsulas.tsx
@@ -11,18 +11,21 @@ type PropsType = {
     etimologias: CapsulasType
 }
 
-const CapsulasComp = ({etimologias}: PropsType) => {
+const CapsulasComp = ({ etimologias }: PropsType) => {
     const [etimilogiaAbierta, setEtimilogiaAbierta] = useState(false)
     return (
         /* Container */
-        <div className="w-full h-full bg-orange-200 relative">
-            <button className="w-full h-1/2 bg-blue-300 z-20 absolute top-0 left-0" 
-            onClick={() => {setEtimilogiaAbierta(!etimilogiaAbierta)}}>{etimologias.palabra}</button>
-            <p className="w-full h-1/2 bg-red-300 z-10 absolute top-0 left-0 p-5" 
-            style={{
-                transform: etimilogiaAbierta ? "translateY(100%)" : "translateY(0%)",
-                transition: "transform 1s ease"
-            }}>{etimologias.etimologia}</p>
+        <div className="w-full h-full overflow-hidden relative">
+            <button className="w-full h-1/2 z-20 absolute top-0 left-0 bg-[url('ImagesCapsulas/capsula.png')] bg-no-repeat bg-cover bg-center"
+                onClick={() => { setEtimilogiaAbierta(!etimilogiaAbierta) }}>{etimologias.palabra}</button>
+            <div className="w-full h-1/2 z-10 absolute top-0 left-0"
+                style={{
+                    transform: etimilogiaAbierta ? "translateY(100%)" : "translateY(0%)",
+                    opacity: etimilogiaAbierta ? "1" : "0",
+                    transition: "transform 1s ease, opacity 1s ease"
+                }}>
+                <p className="w-[80%] mx-auto bg-white p-5 leading-7">{etimologias.etimologia}</p>
+            </div>
         </div>
     )
 }
